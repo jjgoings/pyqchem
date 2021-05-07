9ebbe9e704f9bfc6ffa023c97e0c81b4d3741bb8
Peter Borthwick <73105740+pwborthwick@users.noreply.github.com>
2021-05-07 18:56:15 +0100

    format check

--- routines/mp2.py
+++ routines/mp2.py
@@ -13,9 +13,9 @@ def mp2_energy(dim,Nelec,ints,E):
 
     CC = 0.0
     for i in range(0,Nelec):
-	for j in range(0,Nelec):
-	    for a in range(Nelec,dim*2):
-		for b in range(Nelec,dim*2):
-		    CC += 0.25*(ints[i,j,a,b]*ints[i,j,a,b])/(E[i//2] + E[j//2] - E[a//2] - E[b//2])
+        for j in range(0,Nelec):
+            for a in range(Nelec,dim*2):
+                for b in range(Nelec,dim*2):
+                    CC += 0.25*(ints[i,j,a,b]*ints[i,j,a,b])/(E[i//2] + E[j//2] - E[a//2] - E[b//2])
 
-    return CC
+     return CC
