df738b78ff0120af260da5c2b83fd153aec97914
Peter Borthwick <73105740+pwborthwick@users.noreply.github.com>
2021-05-07 18:54:44 +0100

    format check

--- routines/ccsd.py
+++ routines/ccsd.py
@@ -26,169 +26,169 @@ def ccsd(Nelec,dim,fs,ints,convergence,printops):
     # Initial guess T2
 
     for a in range(Nelec,dim):
-      for b in range(Nelec,dim):
-	for i in range(0,Nelec):
-	  for j in range(0,Nelec):
-	    td[a,b,i,j] += ints[i,j,a,b]/(fs[i,i] + fs[j,j] - fs[a,a] - fs[b,b])
+        for b in range(Nelec,dim):
+            for i in range(0,Nelec):
+                for j in range(0,Nelec):
+                    td[a,b,i,j] += ints[i,j,a,b]/(fs[i,i] + fs[j,j] - fs[a,a] - fs[b,b])
 
     # Make denominator arrays
     Dai = np.zeros((dim,dim))
     for a in range(Nelec,dim):
-      for i in range(0,Nelec):
-	Dai[a,i] = fs[i,i] - fs[a,a]
+        for i in range(0,Nelec):
+            Dai[a,i] = fs[i,i] - fs[a,a]
 
     Dabij = np.zeros((dim,dim,dim,dim))
     for a in range(Nelec,dim):
-      for b in range(Nelec,dim):
-	for i in range(0,Nelec):
-	  for j in range(0,Nelec):
-	    Dabij[a,b,i,j] = fs[i,i] + fs[j,j] - fs[a,a] - fs[b,b]
+        for b in range(Nelec,dim):
+            for i in range(0,Nelec):
+                for j in range(0,Nelec):
+                    Dabij[a,b,i,j] = fs[i,i] + fs[j,j] - fs[a,a] - fs[b,b]
 
     def taus(a,b,i,j):
-      taus = td[a,b,i,j] + 0.5*(ts[a,i]*ts[b,j] - ts[b,i]*ts[a,j])
-      return taus
+        taus = td[a,b,i,j] + 0.5*(ts[a,i]*ts[b,j] - ts[b,i]*ts[a,j])
+        return taus
 
     def tau(a,b,i,j):
-      tau = td[a,b,i,j] + ts[a,i]*ts[b,j] - ts[b,i]*ts[a,j]
-      return tau
+        tau = td[a,b,i,j] + ts[a,i]*ts[b,j] - ts[b,i]*ts[a,j]
+        return tau
 
     def updateintermediates(x):
-      if x == True:
-	Fae = np.zeros((dim,dim))
-	for a in range(Nelec,dim):
-	  for e in range(Nelec,dim):
-	    Fae[a,e] = (1 - (a == e))*fs[a,e]
-	    for m in range(0,Nelec):
-	      Fae[a,e] += -0.5*fs[m,e]*ts[a,m]
-	      for f in range(Nelec,dim):
-		Fae[a,e] += ts[f,m]*ints[m,a,f,e] 
-		for n in range(0,Nelec):
-		  Fae[a,e] += -0.5*taus(a,f,m,n)*ints[m,n,e,f]
+        if x == True:
+            Fae = np.zeros((dim,dim))
+            for a in range(Nelec,dim):
+                for e in range(Nelec,dim):
+                    Fae[a,e] = (1 - (a == e))*fs[a,e]
+                    for m in range(0,Nelec):
+                        Fae[a,e] += -0.5*fs[m,e]*ts[a,m]
+                        for f in range(Nelec,dim):
+                            Fae[a,e] += ts[f,m]*ints[m,a,f,e] 
+                            for n in range(0,Nelec):
+                                Fae[a,e] += -0.5*taus(a,f,m,n)*ints[m,n,e,f]
 
-	Fmi = np.zeros((dim,dim))
-	for m in range(0,Nelec):
-	  for i in range(0,Nelec):
-	    Fmi[m,i] = (1 - (m == i))*fs[m,i]
-	    for e in range(Nelec,dim):
-	      Fmi[m,i] += 0.5*ts[e,i]*fs[m,e]
-	      for n in range(0,Nelec):
-		Fmi[m,i] += ts[e,n]*ints[m,n,i,e] 
-		for f in range(Nelec,dim):
-		  Fmi[m,i] += 0.5*taus(e,f,i,n)*ints[m,n,e,f]
+            Fmi = np.zeros((dim,dim))
+            for m in range(0,Nelec):
+                for i in range(0,Nelec):
+                    Fmi[m,i] = (1 - (m == i))*fs[m,i]
+                    for e in range(Nelec,dim):
+                        Fmi[m,i] += 0.5*ts[e,i]*fs[m,e]
+                        for n in range(0,Nelec):
+                            Fmi[m,i] += ts[e,n]*ints[m,n,i,e] 
+                            for f in range(Nelec,dim):
+                                Fmi[m,i] += 0.5*taus(e,f,i,n)*ints[m,n,e,f]
 
-	Fme = np.zeros((dim,dim))
-	for m in range(0,Nelec):
-	  for e in range(Nelec,dim):
-	    Fme[m,e] = fs[m,e]
-	    for n in range(0,Nelec):
-	      for f in range(Nelec,dim):
-		Fme[m,e] += ts[f,n]*ints[m,n,e,f]
+            Fme = np.zeros((dim,dim))
+            for m in range(0,Nelec):
+                for e in range(Nelec,dim):
+                    Fme[m,e] = fs[m,e]
+                    for n in range(0,Nelec):
+                        for f in range(Nelec,dim):
+                            Fme[m,e] += ts[f,n]*ints[m,n,e,f]
 
-	Wmnij = np.zeros((dim,dim,dim,dim))
-	for m in range(0,Nelec):
-	  for n in range(0,Nelec):
-	    for i in range(0,Nelec):
-	      for j in range(0,Nelec):
-		Wmnij[m,n,i,j] = ints[m,n,i,j]
-		for e in range(Nelec,dim):
-		  Wmnij[m,n,i,j] += ts[e,j]*ints[m,n,i,e] - ts[e,i]*ints[m,n,j,e]
-		  for f in range(Nelec,dim):
-		    Wmnij[m,n,i,j] += 0.25*tau(e,f,i,j)*ints[m,n,e,f]
+            Wmnij = np.zeros((dim,dim,dim,dim))
+            for m in range(0,Nelec):
+                for n in range(0,Nelec):
+                    for i in range(0,Nelec):
+                        for j in range(0,Nelec):
+                            Wmnij[m,n,i,j] = ints[m,n,i,j]
+                            for e in range(Nelec,dim):
+                                Wmnij[m,n,i,j] += ts[e,j]*ints[m,n,i,e] - ts[e,i]*ints[m,n,j,e]
+                                for f in range(Nelec,dim):
+                                    Wmnij[m,n,i,j] += 0.25*tau(e,f,i,j)*ints[m,n,e,f]
 
-	Wabef = np.zeros((dim,dim,dim,dim))
-	for a in range(Nelec,dim):
-	  for b in range(Nelec,dim):
-	    for e in range(Nelec,dim):
-	      for f in range(Nelec,dim):
-		Wabef[a,b,e,f] = ints[a,b,e,f]
-		for m in range(0,Nelec):
-		  Wabef[a,b,e,f] += -ts[b,m]*ints[a,m,e,f] + ts[a,m]*ints[b,m,e,f]
-		  for n in range(0,Nelec):
-		    Wabef[a,b,e,f] += 0.25*tau(a,b,m,n)*ints[m,n,e,f]
+            Wabef = np.zeros((dim,dim,dim,dim))
+            for a in range(Nelec,dim):
+                for b in range(Nelec,dim):
+                    for e in range(Nelec,dim):
+                        for f in range(Nelec,dim):
+                            Wabef[a,b,e,f] = ints[a,b,e,f]
+                            for m in range(0,Nelec):
+                                Wabef[a,b,e,f] += -ts[b,m]*ints[a,m,e,f] + ts[a,m]*ints[b,m,e,f]
+                                for n in range(0,Nelec):
+                                    Wabef[a,b,e,f] += 0.25*tau(a,b,m,n)*ints[m,n,e,f]
 
-	Wmbej = np.zeros((dim,dim,dim,dim))
-	for m in range(0,Nelec):
-	  for b in range(Nelec,dim):
-	    for e in range(Nelec,dim):
-	      for j in range(0,Nelec):
-		Wmbej[m,b,e,j] = ints[m,b,e,j]
-		for f in range(Nelec,dim):
-		  Wmbej[m,b,e,j] += ts[f,j]*ints[m,b,e,f]
-		for n in range(0,Nelec):
-		  Wmbej[m,b,e,j] += -ts[b,n]*ints[m,n,e,j]
-		  for f in range(Nelec,dim):
-		    Wmbej[m,b,e,j] += -(0.5*td[f,b,j,n] + ts[f,j]*ts[b,n])*ints[m,n,e,f]
-
-	return Fae, Fmi, Fme, Wmnij, Wabef, Wmbej
+            Wmbej = np.zeros((dim,dim,dim,dim))
+            for m in range(0,Nelec):
+                for b in range(Nelec,dim):
+                    for e in range(Nelec,dim):
+                        for j in range(0,Nelec):
+                            Wmbej[m,b,e,j] = ints[m,b,e,j]
+                            for f in range(Nelec,dim):
+                                Wmbej[m,b,e,j] += ts[f,j]*ints[m,b,e,f]
+                            for n in range(0,Nelec):
+                                Wmbej[m,b,e,j] += -ts[b,n]*ints[m,n,e,j]
+                                for f in range(Nelec,dim):
+                                    Wmbej[m,b,e,j] += -(0.5*td[f,b,j,n] + ts[f,j]*ts[b,n])*ints[m,n,e,f]
+				
+            return Fae, Fmi, Fme, Wmnij, Wabef, Wmbej
 
     def makeT1(x,ts,td):
-      if x == True:
-	tsnew = np.zeros((dim,dim))
-	for a in range(Nelec,dim):
-	  for i in range(0,Nelec):
-	    tsnew[a,i] = fs[i,a]
-	    for e in range(Nelec,dim):
-	      tsnew[a,i] += ts[e,i]*Fae[a,e]
-	    for m in range(0,Nelec):
-	      tsnew[a,i] += -ts[a,m]*Fmi[m,i]
-	      for e in range(Nelec,dim):
-		tsnew[a,i] += td[a,e,i,m]*Fme[m,e]
-		for f in range(Nelec,dim):
-		  tsnew[a,i] += -0.5*td[e,f,i,m]*ints[m,a,e,f]
-		for n in range(0,Nelec):
-		  tsnew[a,i] += -0.5*td[a,e,m,n]*ints[n,m,e,i]
-	    for n in range(0,Nelec):
-	      for f in range(Nelec,dim): 
-		tsnew[a,i] += -ts[f,n]*ints[n,a,i,f]
-	    tsnew[a,i] = tsnew[a,i]/Dai[a,i]
-      return tsnew
+        if x == True:
+            tsnew = np.zeros((dim,dim))
+            for a in range(Nelec,dim):
+                for i in range(0,Nelec):
+                    tsnew[a,i] = fs[i,a]
+                    for e in range(Nelec,dim):
+                        tsnew[a,i] += ts[e,i]*Fae[a,e]
+                    for m in range(0,Nelec):
+                        tsnew[a,i] += -ts[a,m]*Fmi[m,i]
+                        for e in range(Nelec,dim):
+                            tsnew[a,i] += td[a,e,i,m]*Fme[m,e]
+                            for f in range(Nelec,dim):
+                                tsnew[a,i] += -0.5*td[e,f,i,m]*ints[m,a,e,f]
+                            for n in range(0,Nelec):
+                                tsnew[a,i] += -0.5*td[a,e,m,n]*ints[n,m,e,i]
+                    for n in range(0,Nelec):
+                        for f in range(Nelec,dim): 
+                            tsnew[a,i] += -ts[f,n]*ints[n,a,i,f]
+                     tsnew[a,i] = tsnew[a,i]/Dai[a,i]
+        return tsnew
 
     def makeT2(x,ts,td):
-      if x == True:
-	tdnew = np.zeros((dim,dim,dim,dim))
-	for a in range(Nelec,dim):
-	  for b in range(Nelec,dim):
-	    for i in range(0,Nelec):
-	      for j in range(0,Nelec):
-		tdnew[a,b,i,j] += ints[i,j,a,b]
-		for e in range(Nelec,dim):
-		  tdnew[a,b,i,j] += td[a,e,i,j]*Fae[b,e] - td[b,e,i,j]*Fae[a,e]
-		  for m in range(0,Nelec):
-		    tdnew[a,b,i,j] += -0.5*td[a,e,i,j]*ts[b,m]*Fme[m,e] + 0.5*td[b,e,i,j]*ts[a,m]*Fme[m,e]
-		    continue
-		for m in range(0,Nelec):
-		  tdnew[a,b,i,j] += -td[a,b,i,m]*Fmi[m,j] + td[a,b,j,m]*Fmi[m,i]
-		  for e in range(Nelec,dim):
-		    tdnew[a,b,i,j] += -0.5*td[a,b,i,m]*ts[e,j]*Fme[m,e] + 0.5*td[a,b,j,m]*ts[e,i]*Fme[m,e]
-		    continue
-		for e in range(Nelec,dim):
-		  tdnew[a,b,i,j] += ts[e,i]*ints[a,b,e,j] - ts[e,j]*ints[a,b,e,i]
-		  for f in range(Nelec,dim):
-		    tdnew[a,b,i,j] += 0.5*tau(e,f,i,j)*Wabef[a,b,e,f]
-		    continue
-		for m in range(0,Nelec):
-		  tdnew[a,b,i,j] += -ts[a,m]*ints[m,b,i,j] + ts[b,m]*ints[m,a,i,j]  
-		  for e in range(Nelec,dim):
-		    tdnew[a,b,i,j] += td[a,e,i,m]*Wmbej[m,b,e,j] - ts[e,i]*ts[a,m]*ints[m,b,e,j]
-		    tdnew[a,b,i,j] += -td[a,e,j,m]*Wmbej[m,b,e,i] + ts[e,j]*ts[a,m]*ints[m,b,e,i]
-		    tdnew[a,b,i,j] += -td[b,e,i,m]*Wmbej[m,a,e,j] + ts[e,i]*ts[b,m]*ints[m,a,e,j]
-		    tdnew[a,b,i,j] += td[b,e,j,m]*Wmbej[m,a,e,i] - ts[e,j]*ts[b,m]*ints[m,a,e,i]
-		    continue
-		  for n in range(0,Nelec):
-		    tdnew[a,b,i,j] += 0.5*tau(a,b,m,n)*Wmnij[m,n,i,j]
-		    continue
-		tdnew[a,b,i,j] = tdnew[a,b,i,j]/Dabij[a,b,i,j] 
-	return tdnew
+        if x == True:
+            tdnew = np.zeros((dim,dim,dim,dim))
+            for a in range(Nelec,dim):
+                for b in range(Nelec,dim):
+                    for i in range(0,Nelec):
+                        for j in range(0,Nelec):
+                            tdnew[a,b,i,j] += ints[i,j,a,b]
+                            for e in range(Nelec,dim):
+                                tdnew[a,b,i,j] += td[a,e,i,j]*Fae[b,e] - td[b,e,i,j]*Fae[a,e]
+                                for m in range(0,Nelec):
+                                    tdnew[a,b,i,j] += -0.5*td[a,e,i,j]*ts[b,m]*Fme[m,e] + 0.5*td[b,e,i,j]*ts[a,m]*Fme[m,e]
+                                    continue
+                            for m in range(0,Nelec):
+                                tdnew[a,b,i,j] += -td[a,b,i,m]*Fmi[m,j] + td[a,b,j,m]*Fmi[m,i]
+                                for e in range(Nelec,dim):
+                                    tdnew[a,b,i,j] += -0.5*td[a,b,i,m]*ts[e,j]*Fme[m,e] + 0.5*td[a,b,j,m]*ts[e,i]*Fme[m,e]
+                                    continue
+                            for e in range(Nelec,dim):
+                                tdnew[a,b,i,j] += ts[e,i]*ints[a,b,e,j] - ts[e,j]*ints[a,b,e,i]
+                                for f in range(Nelec,dim):
+                                    tdnew[a,b,i,j] += 0.5*tau(e,f,i,j)*Wabef[a,b,e,f]
+                                    continue
+                            for m in range(0,Nelec):
+                                tdnew[a,b,i,j] += -ts[a,m]*ints[m,b,i,j] + ts[b,m]*ints[m,a,i,j]  
+                                for e in range(Nelec,dim):
+                                    tdnew[a,b,i,j] += td[a,e,i,m]*Wmbej[m,b,e,j] - ts[e,i]*ts[a,m]*ints[m,b,e,j]
+                                    tdnew[a,b,i,j] += -td[a,e,j,m]*Wmbej[m,b,e,i] + ts[e,j]*ts[a,m]*ints[m,b,e,i]
+                                    tdnew[a,b,i,j] += -td[b,e,i,m]*Wmbej[m,a,e,j] + ts[e,i]*ts[b,m]*ints[m,a,e,j]
+                                    tdnew[a,b,i,j] += td[b,e,j,m]*Wmbej[m,a,e,i] - ts[e,j]*ts[b,m]*ints[m,a,e,i]
+                                    continue
+                                for n in range(0,Nelec):
+                                    tdnew[a,b,i,j] += 0.5*tau(a,b,m,n)*Wmnij[m,n,i,j]
+                                    continue
+                                tdnew[a,b,i,j] = tdnew[a,b,i,j]/Dabij[a,b,i,j] 
+        return tdnew
 
     def ccsdenergy():
-      ECCSD = 0.0
-      for i in range(0,Nelec):
-	for a in range(Nelec,dim):
-	  ECCSD += fs[i,a]*ts[a,i]
-	  for j in range(0,Nelec):
-	    for b in range(Nelec,dim):
-	      ECCSD += 0.25*ints[i,j,a,b]*td[a,b,i,j] + 0.5*ints[i,j,a,b]*(ts[a,i])*(ts[b,j]) 
-      return ECCSD
+        ECCSD = 0.0
+        for i in range(0,Nelec):
+            for a in range(Nelec,dim):
+                ECCSD += fs[i,a]*ts[a,i]
+                for j in range(0,Nelec):
+                    for b in range(Nelec,dim):
+                        ECCSD += 0.25*ints[i,j,a,b]*td[a,b,i,j] + 0.5*ints[i,j,a,b]*(ts[a,i])*(ts[b,j]) 
+        return ECCSD
 
     # CCSD iteration
     do_DIIS = True 
